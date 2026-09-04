c53e4967bf4799f6f390c6d00e1591d3851d8f2f
eponcem <ernestomaximiliano03@gmail.com>
2026-09-04 13:19:35 -0400

    visualizacion: agregar grafico 2 de variacion mensual y ejecucion del modulo
    Implementada graficar_cancelacion_mensual con barras de error propagado mes a mes y configurado el bloque main para procesar y registrar A3 y A4.

--- src/anualidad.py
+++ src/anualidad.py
@@ -51,3 +51,27 @@ def analizar_variacion_anual_a4(valores, directorio):
     escribir_seccion_csv(ruta_csv, 'a', '--- A4: variacion enero-diciembre por anio (mas confiable primero) ---',
     ['anio', 'variacion_clp', 'error_absoluto', 'error_relativo_pct'], filas,)
 
+def graficar_cancelacion_mensual(valores, etiquetas, directorio):
+    val_aprox = v_redondear(valores, 2)
+    deltas_mes = np.diff(val_aprox) 
+    ea_mes = np.abs(valores - val_aprox)
+    ea_propagado_mes = ea_mes[1:] + ea_mes[:-1] 
+    
+    plt.figure(figsize=(10, 4))
+    plt.bar(etiquetas[1:], deltas_mes, yerr=ea_propagado_mes)
+    plt.axhline(0, color='black')
+    plt.xticks(rotation=90, fontsize=7)
+    plt.title("Variacion Mes a Mes")
+    plt.tight_layout()
+    plt.savefig(os.path.join(directorio, '../graficos/2_cancelacion_mensual.png'))
+    plt.close()
+
+if __name__ == '__main__':
+    directorio_actual = os.path.dirname(__file__)
+    ruta_csv = os.path.join(directorio_actual, '../data/dolar_observado_sii_2022_2025.csv')
+    
+    valores, etiquetas = cargar_csv(ruta_csv)
+    
+    analizar_cancelacion_a3(valores, directorio_actual)
+    analizar_variacion_anual_a4(valores, directorio_actual)
+    graficar_cancelacion_mensual(valores, etiquetas, directorio_actual)
