a22398b88c23772d44bce2ae04b3d9b3274eb783
eponcem <ernestomaximiliano03@gmail.com>
2026-09-04 10:37:03 -0400

    A5: calcular rentabilidad optima y generar grafico 4 con barras de error
    Agregada la busqueda de minimo y maximo historico, exportacion de rentabilidad a meses posteriores en CSV, grafico de errorbar y bloque de ejecucion principal del modulo.

--- src/errores.py
+++ src/errores.py
@@ -85,4 +85,59 @@ def evaluar_compra_venta(precio_compra_real, precio_venta_real, monto=1000000):
     return {'p_compra_aprox': p_compra_aprox, 'p_venta_aprox': p_venta_aprox,'usd': usd, 'ganancia': ganancia, 'ea_ganancia': ea_ganancia,
     'rentabilidad': rentabilidad, 'error_rentabilidad': error_rentabilidad,}
 
+def analizar_compra_venta(valores, etiquetas, directorio):
+    monto = 1000000
+    idx_min = np.argmin(valores)
+    idx_max = np.argmax(valores)
+    p_compra_real = valores[idx_min]
+
+    er_compra = np.abs(p_compra_real - v_redondear(p_compra_real, 2)) / p_compra_real * 100
+
+    meses_venta = valores[idx_min+1:]
+    etiq_venta = etiquetas[idx_min+1:]
+    p_venta_aprox = v_redondear(meses_venta, 2)
+
+    er_venta = np.abs(meses_venta - p_venta_aprox) / meses_venta * 100
+
+    usd = monto / v_redondear(p_compra_real, 2)
+    pesos_final = usd * p_venta_aprox
+    ganancia = pesos_final - monto
+
+    er_pesos_final = er_compra + er_venta 
+    ea_ganancia = (er_pesos_final / 100) * pesos_final 
     
+    rentabilidad = (ganancia / monto) * 100
+    error_rentabilidad = (ea_ganancia / monto) * 100
+    
+    # Grafico 4
+    plt.figure(figsize=(10, 4))
+    plt.errorbar(etiq_venta, rentabilidad, yerr=error_rentabilidad, fmt='o-')
+    plt.axhline(0, color='black')
+    plt.xticks(rotation=90, fontsize=7)
+    plt.title("Rentabilidad desde el Minimo")
+    plt.tight_layout()
+    plt.savefig(os.path.join(directorio, '../graficos/4_rentabilidad.png'))
+    plt.close()
+
+    r_optimo = evaluar_compra_venta(valores[idx_min], valores[idx_max])
+    conclusion_solida = bool(abs(r_optimo['ganancia']) > r_optimo['ea_ganancia'])
+
+    ruta_csv = os.path.join(directorio, '../evaluacion_errores.csv')
+
+    filas_tabla = zip(etiq_venta, np.round(meses_venta, 2), np.round(ganancia, 2),
+                       np.round(ea_ganancia, 2), np.round(er_pesos_final, 2))
+    escribir_seccion_csv(ruta_csv, 'a','--- A5 (detalle grafico 4): comprar en el minimo, vender en cada mes posterior ---',
+    ['mes_venta', 'precio_venta_real', 'ganancia_clp', 'error_absoluto_clp', 'error_relativo_pct'],filas_tabla,)
+
+    escribir_seccion_csv(ruta_csv, 'a','--- A5 (respuesta): comprar en el minimo, vender en el maximo del periodo ---',
+    ['mes_compra', 'mes_venta', 'rentabilidad_pct', 'error_pct', 'conclusion_solida'],[[etiquetas[idx_min], etiquetas[idx_max], round(r_optimo['rentabilidad'], 2),
+    round(r_optimo['error_rentabilidad'], 2), conclusion_solida]],)
+
+if __name__ == '__main__':
+    directorio_actual = os.path.dirname(__file__)
+    ruta_csv_datos = os.path.join(directorio_actual, '../data/dolar_observado_sii_2022_2025.csv')
+    valores, etiquetas = cargar_csv(ruta_csv_datos)
+    
+    analizar_error_representacion(valores, etiquetas, directorio_actual)
+    analizar_ejemplo_a2(valores, etiquetas, directorio_actual)
+    analizar_compra_venta(valores, etiquetas, directorio_actual)
